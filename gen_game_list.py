782ea2830526ab9176e86ab2897f605fe7c24e91
Mauro Severin <mseverin@alpenite.com>
2024-10-21 02:53:00 +0200

    Total refactor

--- gen_game_list.py
+++ gen_game_list.py
@@ -1,81 +1,96 @@
-import os, shutil
+import os, shutil, itertools, xml.etree.ElementTree, re
 from pathlib import Path
 from PIL import Image
-os.system('clear')
-shutil.rmtree('tf', ignore_errors=True)
-print ('Remove tf folder')
+from urllib.request import urlretrieve
 
 #set vars
 romsfolder = 'source/roms'
 console = 'neogeo'
 
-#create folder
-os.makedirs('tf/game/' + console.upper())
-os.makedirs('tf/settings/res/' + console.upper() + '/string')
-os.makedirs('tf/settings/res/' + console.upper() + '/pic')
+#init folders
+print ('Remove tf folder')
+shutil.rmtree('tf', ignore_errors=True)
+os.makedirs(f'tf/game/{console.upper()}')
+os.makedirs(f'tf/settings/res/{console.upper()}/string')
+os.makedirs(f'tf/settings/res/{console.upper()}/pic')
+
+#init realnames
+os.system('clear')
+urlretrieve('https://raw.githubusercontent.com/RetroPie/EmulationStation/master/resources/mamenames.xml', 'mamenames.xml')
+with open('mamenames.xml') as norootfile:
+    rootfile = itertools.chain('<root>', norootfile, '</root>')
+    mamenamexml = xml.etree.ElementTree.fromstringlist(rootfile)
 
 #generate gamelist
 gamelist = []
-gamefile = ''
+reallist = []
 for gamefile in os.listdir(romsfolder + '/' + console):
     if gamefile.endswith('.zip') or gamefile.endswith('.7z'):
         gamelist.append(gamefile)
-gamelist.sort()
-gamelistcount = len(gamelist)
-print ('Found ' + str(gamelistcount) + ' games')
+        gamename = os.path.splitext(gamefile)[0]
+        mamenametag = mamenamexml.find(f'.//*[mamename=\'{gamename}\']')
+        reallist.append(re.sub(' +', ' ', re.sub('[^A-Za-z0-9 ]+', '', mamenametag.find('realname').text))[:28])
+gamedict = dict(zip(reallist,gamelist))
+gamedict = dict(sorted((key,value) for (key,value) in gamedict.items()))
+gamedictcount = len(gamedict)
+print (f'Found {gamedictcount} games')
 print ()
 
-#init outfile
+#init outfilexml
 pagecount = 1
 itemcount = 0
-outfile = '<?xml version="1.0" encoding="utf-8"?>\r\n<strings_resources>\r\n'
-outfile = outfile + '  <icon_para game_list_total="' + str(gamelistcount) + '"/>\r\n'
+outfilexml = '<?xml version="1.0" encoding="utf-8"?>\r\n<strings_resources>\r\n'
+outfilexml = outfilexml + f'  <icon_para game_list_total="{gamedictcount}"/>\r\n'
 
 #manage game
-gamefile = ''
-for gamefile in gamelist:
+picnotfound = 0
+for realname, gamefile in gamedict.items():
+    print (f'Magane game\t> {realname}')
+    
     #copy file
-    print ('Copy game\t> ' + romsfolder + '/' + console + '/' + gamefile)
-    shutil.copy(romsfolder + '/' + console + '/' + gamefile, 'tf/game/' + console.upper() + "/")
-    gamename = os.path.splitext(gamefile)[0]
+    print (f'Copy game\t> {romsfolder}/{console}/{gamefile}')
+    shutil.copy(f'{romsfolder}/{console}/{gamefile}', f'tf/game/{console.upper()}/')
 
     #find image
+    gamename = os.path.splitext(gamefile)[0]
     picfile = ''
-    for picimg in Path(romsfolder + '/' + console).rglob(gamename + '-image*'):
+    for picimg in Path(f'{romsfolder}/{console}').rglob(f'{gamename}-image*'):
         picfile = str(picimg)
     if not picfile:
-        for picfile in Path(romsfolder + '/' + console).rglob(gamename + '-thumb*'):
+        for picfile in Path(f'{romsfolder}/{console}').rglob(f'{gamename}-thumb*'):
             picfile = str(picfile)
     
     #copy image
     if not picfile:
         print ('\x1b[0;31;40m' + 'No image' + '\x1b[0m')
+        picnotfound +=1
     elif 'jpg' in picfile:
-        print ('Convert image\t> ' + picfile)
+        print (f'Convert image\t> {picfile}')
         picjpg = Image.open(picfile)
         picfile = picfile.replace(".jpg", ".png")
         picjpg.save(picfile)
-        print ('Move image\t> ' + picfile)
-        shutil.move(picfile, 'tf/settings/res/' + console.upper() + '/pic/' + gamename + '.png' )
+        print (f'Move image\t> {picfile}')
+        shutil.move(picfile, f'tf/settings/res/{console.upper()}/pic/{gamename}.png')
     else:
-        print ('Copy image\t> ' + picfile)
-        shutil.copy(picfile, 'tf/settings/res/' + console.upper() + '/pic/' + gamename + '.png' )
+        print (f'Copy image\t> {picfile}')
+        shutil.copy(picfile, f'tf/settings/res/{console.upper()}/pic/{gamename}.png')
     print ()
 
-    #page outfile
+    #page outfilexml
     if (itemcount == 0):
-        outfile = outfile + '  <icon_page' + str(pagecount) + '>\r\n'
-    outfile = outfile + '      <icon' + str(itemcount) + '_para id="' + console.upper() + '" name=\"' + gamename.capitalize() + '\" game_path=\"' + gamename + ".zip" + '\"/>\r\n'
-    if (itemcount == 9 or gamefile == gamelist[-1]):
-        outfile = outfile + '  </icon_page' + str(pagecount) + '>\r\n'
+        outfilexml = outfilexml + f'  <icon_page{str(pagecount)}>\r\n'
+    outfilexml = outfilexml + f'      <icon{str(itemcount)}_para id="{console.upper()}" name="{realname}" game_path="{gamename}.zip"/>\r\n'
+    if (itemcount == 9 or realname == list(gamedict)[-1]):
+        outfilexml = outfilexml + f'  </icon_page{str(pagecount)}>\r\n'
         itemcount = -1
         pagecount +=1
     itemcount +=1
 
-# end outfile
-outfile = outfile + '</strings_resources>'
+# end outfilexml
+outfilexml = outfilexml + '</strings_resources>'
 
-#save outfile
-print ('Save config\t> tf/settings/res/' + console.upper() + '/string/game_strings_en.xml')
-with open('tf/settings/res/' + console.upper() + '/string/game_strings_en.xml', 'w') as xmlfile:
-    xmlfile.write(outfile)
+#save outfilexml
+print (f'No image games\t> {picnotfound}')
+print (f'Save config\t> tf/settings/res/{console.upper()}/string/game_strings_en.xml')
+with open(f'tf/settings/res/{console.upper()}/string/game_strings_en.xml', 'w') as xmlfile:
+    xmlfile.write(outfilexml)
